f50ad7d0f23ffe687f2f10efa2beffb8155b3c01
golith <golith@hotmail.com>
2018-06-01 09:08:23 +0930

    add css

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html>
 	<head>
 		<title>IT System Design | Home Page</title>
+		<!-- link to main stylesheet -->
+		<link rel="stylesheet" type="text/css" href="/css/main.css">
 	</head>
 	<body>
 		<nav>
