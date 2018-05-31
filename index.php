314a802120e8b3579be78ce04d989fd92bfc3f2c
golith <golith@hotmail.com>
2018-06-01 09:05:06 +0930

    index page

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>IT System Design | Home Page</title>
+	</head>
+	<body>
+		<nav>
+    		<ul>
+        		<li><a href="/">Home</a></li>
+	        	<li><a href="/about">About</a></li>
+        		<li><a href="/cv">CV</a></li>
+        		<li><a href="/blog">Services</a></li>
+    		</ul>
+		</nav>
+		<div class="container">
+    		<div class="blurb">
+        		<h1>ITSD Home Page!</h1>
+				<p>We aprovide quality srvice to the public of adelaide. <a href="/about">Read more about our services...</a></p>
+    		</div><!-- /.blurb -->
+		</div><!-- /.container -->
+		<footer>
+    		<ul>
+        		<li><a href="mailto:scot.henderson@outlook.com">email</a></li>
+        		<li><a href="https://github.com/golith">github.com/golith</a></li>
+			</ul>
+		</footer>
+	</body>
+</html>
+
